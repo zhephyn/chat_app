f7b347a9438c0b07cd325f6159edeed400cb9bdc
zhephyn <zhephynwinter@gmail.com>
2024-07-27 01:50:43 +0300

    setting up associations for direct messaging and group messaging in Action Cable

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
     has_many :messages
+    has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :messages 
+  has_many :groups
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
