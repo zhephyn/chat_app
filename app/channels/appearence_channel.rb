0f184408c2929a382b93badc43a70720fd11826a
zhephyn <zhephynwinter@gmail.com>
2024-08-07 10:57:29 +0300

    updated the recieve function to update the list in realtime when the a new user comes online

--- app/channels/appearence_channel.rb
+++ app/channels/appearence_channel.rb
@@ -1,9 +1,16 @@
 class AppearenceChannel < ApplicationCable::Channel
   def subscribed
     current_user.appear
+    broadcast_online_users
   end
 
   def unsubscribed
     current_user.disappear
+    broadcast_online_users
+  end
+
+  private
+  def broadcast_online_users
+    ActionCable.server.broadcast("appearence_channel", users: User.online)
   end
 end
--- app/javascript/channels/appearence_channel.js
+++ app/javascript/channels/appearence_channel.js
@@ -10,5 +10,12 @@ consumer.subscriptions.create("AppearenceChannel", {
   },
 
   received(data) {
+    const onlineUsersList = document.getElementById('online-users');
+    onlineUsersList.innerHTML = '';
+    data.users.forEach(user => {
+      const userElement = document.createElement('li');
+      userElement.innerText = user.email;
+      onlineUsersList.appendChild(userElement);
+    });
   }
 });
